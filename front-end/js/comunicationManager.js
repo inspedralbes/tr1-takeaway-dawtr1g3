108a57bb2d1aa7317b2045fcfef2450e9d29daf2
julsluks <a22betvilver@inspedralbes.cat>
2023-10-25 00:17:45 +0200

    landing, nav, footer creant shop-page

--- front-end/js/comunicationManager.js
+++ front-end/js/comunicationManager.js
@@ -1,6 +1,6 @@
 export async function getProductes() {
-    let response = await fetch("https://www.omdbapi.com/?s=cars&apikey=8c3be63e");
+    let response = await fetch("http://localhost:8000/api/productes");
     let productes = await response.json();
     console.log(productes);
-    return productes.Search;
+    return productes;
 }
