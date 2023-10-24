874b8e685f593d7562d6d505ddfe3f66c2f5252c
julsluks <a22betvilver@inspedralbes.cat>
2023-10-24 09:28:42 +0200

    destroy, update, search de categories

--- laravel-api/app/Http/Controllers/CategoriesController.php
+++ laravel-api/app/Http/Controllers/CategoriesController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    //
+
+    public function update(Request $request, string $id)
+    {
+        $categoria = Categoria::find($id);
+        $categoria->update($request->all());
+        return $categoria;
+    }
+
+    public function destroy(string $id)
+    {
+        return Categoria::destroy($id);
+    }
+
+    public function search(string $nom)
+    {
+        return Categoria::where('nom', 'like', '%'.$nom.'%')->get();
+    }
 }
--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -1,15 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductesController;
-
-Route::get("/products/search/{title}", [ProductsController::class,"search"]);
-Route::put("/products/{id}", [ProductsController::class,"update"]);
-Route::delete("/products/{id}", [ProductsController::class,"destroy"]);
 
 Route::resource('productes',ProductesController::class);
+
+Route::get('/categories/search/{nom}', [CategoriesController::class, 'search']);
+
+Route::resource('categories', CategoriesController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
