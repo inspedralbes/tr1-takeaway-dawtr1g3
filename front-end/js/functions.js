a8e4377be0ccfe79a6763fd694ed3b1900133d34
julsluks <a22betvilver@inspedralbes.cat>
2023-10-25 11:01:16 +0200

    ticket

--- front-end/js/functions.js
+++ front-end/js/functions.js
@@ -68,20 +68,34 @@ createApp({
         },
         clickButtonMore(producte) {
             let position = this.searchProductePos(producte);
-            console.log(position);
-            console.log('here');
             this.productes[position].counter++;
-            console.log('hola');
             if (!this.shopping_cart.products_cart.includes(this.productes[position])) {
-                console.log('aaaa');
                 this.shopping_cart.products_cart.push(this.productes[position]);
-                console.log('ppppp');
             }
             //shopping_cart
             this.countPriceAccount();
             this.countItemsAccount();
-            //views
-            // this.showTotals();
+        },
+        //ticket
+        searchProducte_CartPos(producte_cart) {
+            for (let i = 0; i < this.shopping_cart.products_cart.length; i++) {
+                if(producte_cart.id == this.shopping_cart.products_cart[i].id){
+                    return i;
+                }
+            }
+        },
+        clickButtonLess(producte_cart) {
+            let position = this.searchProductePos(producte_cart);
+            if (this.productes[position].counter > 0) {
+                this.productes[position].counter--;
+                if (this.productes[position].counter == 0) {
+                    let index = this.searchProducte_CartPos(producte_cart);
+                    this.shopping_cart.products_cart.splice(index,1);
+                }
+                //shopping_cart
+                this.countPriceAccount();
+                this.countItemsAccount();
+            }
         }
         //checkout-page_functions
 
