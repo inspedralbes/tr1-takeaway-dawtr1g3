e9660b287217c658ff49df165332197678c2b444
a22hugtrigon <a22hugtrigon@inspedralbes.cat>
2023-10-26 13:18:30 +0200

    nav_comandes_admin

--- front-end/js/functions.js
+++ front-end/js/functions.js
@@ -53,21 +53,21 @@ createApp({
         //shop-page_functions
         searchProductePos(producte) {
             for (let i = 0; i < this.productes.length; i++) {
-                if(producte.id == this.productes[i].id){
+                if (producte.id == this.productes[i].id) {
                     return i;
                 }
             }
         },
         countPriceAccount() {
             let priceSCart = 0;
-            this.shopping_cart.products_cart.forEach( element => {
+            this.shopping_cart.products_cart.forEach(element => {
                 priceSCart += element.preu * element.counter;
             });
             this.shopping_cart.totalPrice = priceSCart;
         },
         countItemsAccount() {
             let itemsSCart = 0;
-            this.shopping_cart.products_cart.forEach( element => {
+            this.shopping_cart.products_cart.forEach(element => {
                 itemsSCart += element.counter;
             });
             this.shopping_cart.totalItems = itemsSCart;
@@ -85,7 +85,7 @@ createApp({
         //ticket
         searchProducte_CartPos(producte_cart) {
             for (let i = 0; i < this.shopping_cart.products_cart.length; i++) {
-                if(producte_cart.id == this.shopping_cart.products_cart[i].id){
+                if (producte_cart.id == this.shopping_cart.products_cart[i].id) {
                     return i;
                 }
             }
@@ -96,7 +96,7 @@ createApp({
                 this.productes[position].counter--;
                 if (this.productes[position].counter == 0) {
                     let index = this.searchProducte_CartPos(producte_cart);
-                    this.shopping_cart.products_cart.splice(index,1);
+                    this.shopping_cart.products_cart.splice(index, 1);
                 }
                 //shopping_cart
                 this.countPriceAccount();
@@ -106,14 +106,23 @@ createApp({
                 }
             }
         },
-        clickBuyButton(){
+        clickBuyButton() {
             this.shop_page = false;
             this.cart_toggle = false;
             this.checkout_page = true;
-        }
+        },
         //checkout-page_functions
 
         //status-page_functions
+
+clickOrdersButton() {
+    this.landing_page = false;
+    this.shop_page=false;
+    this.cart_toggle=false;
+    this.checkout_page = false;
+    this.status_page=true;
+
+        }
     },
     created() {
         getProductes().then((productes) => {
